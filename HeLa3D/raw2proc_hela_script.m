39b4e25c9800a795455d4fdb791f7e846bff6ecd
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2013-09-08 01:12:26 -0400

    Recycled variables and removed workspace clearing call

--- HeLa3D/raw2proc_hela_script.m
+++ HeLa3D/raw2proc_hela_script.m
@@ -1,13 +1,8 @@
-clear all
 close all
 
 % some cells in here were left out because they have imaging errors
 
-%icaoberg 9/8/2013
-%not needed it. we inherited from master script
-%imgloc = [];  % location of HeLa 3D images, e.g. imgloc = '../images/Hela';
-
 for I = [1:4 6:42 44:52]
 	disp( ['Preprocessing image: ' num2str(i) ] )
-	raw2proc_hela(I, imgloc);
+	raw2proc_hela( I, imgpath );
 end
