3e99f301583f3b994aed25522c68de896f046e36
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2013-09-08 17:54:06 -0400

    Updated scripts

--- HeLa3D/auto_segment_hela_script.m
+++ HeLa3D/auto_segment_hela_script.m
@@ -1,6 +1,6 @@
 close all
 
 for cellnum = [1 2 4 6 10 12:42 46:49 51:52]
-	disp( ['Segmenting image: ' num2str(I) ] )
+	disp( ['Segmenting image: ' num2str(cellnum) ] )
 	auto_segment_hela(cellnum);
 end
--- HeLa3D/extract_features_script.m
+++ HeLa3D/extract_features_script.m
@@ -3,6 +3,6 @@ close all
 batchno = 1;
 
 for cellnum = [1 2 4 6 10 12:42 46:49 51:52]
-	disp( ['Extracting features from image: ' num2str(I) ] )
+	disp( ['Extracting features from image: ' num2str(cellnum) ] )
 	all_script(cellnum,batchno);
 end
