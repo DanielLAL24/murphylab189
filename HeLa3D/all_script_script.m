3a95976cad8f714717a19d29f64e3cd037a59c9c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2013-09-08 18:07:29 -0400

    Updated scripts

--- HeLa3D/all_script_script.m
+++ HeLa3D/all_script_script.m
@@ -3,8 +3,8 @@ close all
 
 batchno = 1;
 
-
 for cellnum = [1 2 4 6 10 12:42 46:49 51:52]
-	mainScript10_hela_acute_script(cellnum,batchno);
-%	all_script(cellnum,batchno);
+	disp(['(Batch number, cell number):(' num2str(batchno) ',' num2str(cellnum) ')'])
+	mainScript10_hela_acute_script( cellnum, batchno );
+    %all_script(cellnum,batchno);
 end
